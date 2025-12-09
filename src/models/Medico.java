2b57e84fa84b856a6baf56e6a74a8087702da907
Capim32 <victorsbn3@gmail.com>
2025-12-09 18:54:22 -0300

    base de pacientes e médicos

--- src/models/Medico.java
+++ src/models/Medico.java
@@ -1,5 +1,41 @@
 package models;
 
 public class Medico {
-    
+    protected String nome;
+    protected int id;
+    protected String especialidade;
+    protected int planoDeSaude;
+    protected int atendimentos;
+
+    public Medico(String nome) {
+        this.nome = nome;
+        this.id = id;
+        this.especialidade = especialidade;
+        this.planoDeSaude = planoDeSaude;
+        this.atendimentos = atendimentos;
+    }
+    // talvez seja interessante conseguir puxar o médico pelo ID
+    public Medico (int id) {
+        this.nome = nome;
+        this.id = id;
+        this.especialidade = especialidade;
+        this.planoDeSaude = planoDeSaude;
+        this.atendimentos = atendimentos;
+    }
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+
+    public String getEspecialidade() {return especialidade;}
+    public void setEspecialidade(String especcialidade) {this.especialidade = especcialidade;}
+
+    public int getPlanoDeSaude() {return planoDeSaude;}
+    public void setPlanoDeSaude(int planoDeSaude) {this.planoDeSaude = planoDeSaude;}
+
+    public int getAtendimentos() {return atendimentos;}
+    public void setAtendimentos(int atendimentos) {this.atendimentos = atendimentos;}
+
 }
--- src/models/Paciente.java
+++ src/models/Paciente.java
@@ -1,5 +1,29 @@
 package models;
 
 public class Paciente {
+    protected String nome;
+    protected int id;
+    protected int planoDeSaude;
     
+    public Paciente(String nome) {
+        this.nome = nome;
+        this.id = id;
+        this.planoDeSaude = planoDeSaude;
+    }
+    // talvez seja interessante conseguir puxar o médico pelo ID
+    public Paciente (int id) {
+        this.nome = nome;
+        this.id = id;
+        this.planoDeSaude = planoDeSaude;
+    }
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+
+    public int getPlanoDeSaude() {return planoDeSaude;}
+    public void setPlanoDeSaude(int planoDeSaude) {this.planoDeSaude = planoDeSaude;}
+
 }
